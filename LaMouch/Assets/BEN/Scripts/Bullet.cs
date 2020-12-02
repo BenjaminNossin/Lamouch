114237bca03d48962317f079a8b770bdbbfafde8
Benjamin <f.nossin@rubika-edu.com>
2020-12-02 14:53:02 +0100

    importing first prototype

---
+++ LaMouch/Assets/BEN/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField, Range(0.5f, 5f)] private float speed = 2f;
+    [SerializeField, Range(0.25f, 5f)] private float destroyDelay = 2f;
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyDelay); 
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward.normalized * Time.fixedDeltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.transform.parent.gameObject);
+            Destroy(gameObject, 0.1f); 
+        }
+    }
+}
---
+++ LaMouch/Assets/BEN/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform firepoint;
+    [SerializeField, Range(0f, 1f)] private float delay = 0.75f;
+
+    private bool canShoot = true; 
+
+    public void Attack()
+    {
+        if (canShoot)
+        {
+            Instantiate(bullet, firepoint.position, firepoint.rotation);
+            StartCoroutine(Cooldown()); 
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        canShoot = false; 
+
+        yield return new WaitForSeconds(delay);
+        canShoot = true; 
+    }
+}
