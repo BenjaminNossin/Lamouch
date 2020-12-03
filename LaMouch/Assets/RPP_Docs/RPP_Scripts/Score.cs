6e8f18ac7d209e6e3ad4cd6e9875ce01329af63f
Benjamin <f.nossin@rubika-edu.com>
2020-12-03 10:03:11 +0100

    integrating UI (raph)

---
+++ LaMouch/Assets/RPP_Docs/RPP_Scripts/Score.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    public Text score, lives;
+    public static int scoreInt, livesInt;
+    public bool isInEndScene = false;
+
+    private void Start()
+    {
+        if (!isInEndScene)
+        {
+            scoreInt = 0;
+        }        
+        livesInt = 3;
+    }
+
+    void Update()
+    {
+        if (!isInEndScene)
+        {
+            score.text = ": " + scoreInt;
+            lives.text = ": " + livesInt;
+        }  
+        
+        else
+        {
+            score.text = "" + scoreInt;
+        }
+
+        if (livesInt <= 0)
+        {
+            SceneManager.LoadScene("BestScoreScene");
+        }
+    }
+
+    public void AddScore()
+    {
+        scoreInt ++;
+    }
+
+    public void LoseLIfe()
+    {
+        livesInt--;
+    }
+}
