8093c63411103304cee00f7e01c017dc254819f6
Benjamin <f.nossin@rubika-edu.com>
2020-12-03 11:45:33 +0100

    spawning

--- LaMouch/Assets/Plugins/Scripts/EnemyAI.cs
+++ LaMouch/Assets/Plugins/Scripts/EnemyAI.cs
@@ -5,16 +5,24 @@ public enum State { None, Idle, Rush }
 // run around pillars for x seconds before rushing to player with x speed
 public class EnemyAI : MonoBehaviour
 {
-    [Range(1f, 40)] public byte speed = 20; 
+    [Range(1f, 40)] public byte speed = 20;
+    public State AIState;
 
+    private Transform pillar;
     // run around
     // rush speed
-    public State AIState;
-    public Transform pillar; 
+
+    private void Start()
+    {
+        pillar = GameObject.Find("Cursed_Pilier").transform; // :D
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.RotateAround(pillar.position, Vector3.up, speed * Time.fixedDeltaTime); 
+        transform.RotateAround(pillar.position, Vector3.up, speed * Time.fixedDeltaTime);
+        AIState = State.Idle;
+
+        transform.Rotate(Vector3.up, -2.5f * Time.fixedDeltaTime); 
     }
 }
