7387970fd7dfb10e2fd93f245effb40add86bc1b
Wardreyk <b.bohez@rubika-edu.com>
2020-12-04 15:27:27 +0100

    Game Mode selections

---
+++ LaMouch/Assets/BAPT/BAPT_Script/Game_Mode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Mode : MonoBehaviour
+{
+    public static bool withScan;
+
+    public void ActiteScan()
+    {
+        withScan = true;
+        Debug.Log("Scan Activer");
+    }
+
+    public void DesactiveScan()
+    {
+        withScan = false;
+        Debug.Log("Scan désactiver");
+    }
+}
